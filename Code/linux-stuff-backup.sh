4e0b9b7d630a7d32f8fe8eb8c9ffeaed325ea460
sfrice1031 <sfrice1031@gmail.com>
2019-06-01 07:57:03 -0400

    Script Update

--- Code/linux-stuff-backup.sh
+++ Code/linux-stuff-backup.sh
@@ -8,7 +8,7 @@ cp ~/.zshrc ~/Git/Linux-Stuff/Arch-Stuff
 cp ~/.config/i3/config ~/Git/Linux-Stuff/Arch-Stuff
 cp ~/.vimrc ~/Git/Linux-Stuff/Arch-Stuff
 cp -r ~/.config/qutebrowser ~/Git/Linux-Stuff/Arch-Stuff
-cp -r ~/Git/suckless  ~/Git/Linux-Stuff/Arch-Stuff
+cp  ~/.xinitrc  ~/Git/Linux-Stuff/Arch-Stuff
 
 # Copying local code and scripts into their respective repo folders
 cp ~/Scripts/linux-stuff-backup.sh ~/Git/Linux-Stuff/Code
