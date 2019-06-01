4d70804a88c1ef47e758729e4611fc2f6d842d0d
sfrice1031 <sfrice1031@gmail.com>
2019-06-01 11:13:15 -0400

    Script Update

--- Code/linux-stuff-backup.sh
+++ Code/linux-stuff-backup.sh
@@ -30,7 +30,7 @@ cd ~/Git/Linux-Stuff
 # Update and merge with the repo on github
 git add Arch-Stuff Code
 git commit -m "Script Update" 
-git push 
+git push && clear 
 
 # Bob Dobbs respects the 5-10 minutes of slack you've gained back by using this script
 cat << "EOF" 
